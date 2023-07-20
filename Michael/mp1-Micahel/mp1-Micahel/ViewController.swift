abf62684967ec1f17f77eba21b5d688adbd2379d
MichaelOpppong <140095574+MichaelOpppong@users.noreply.github.com>
2023-07-20 11:44:25 -0400

    Add files via upload

---
+++ Michael/mp1-Micahel/mp1-Micahel/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mp1-Micahel
+//
+//  Created by SMART Scholars on 7/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Fact1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
